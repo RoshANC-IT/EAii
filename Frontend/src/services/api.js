33893f8cbab41659399db81cd104b427e31eb2e9
Roshan Chauhan <roshanranjput92@gmail.com>
2025-04-13 12:20:16 +0530

    api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api', // Correct base URL for API
+  baseURL: 'https://eaii.onrender.com/api', // Correct base URL for API
   headers: {
     'Content-Type': 'application/json',
   },
