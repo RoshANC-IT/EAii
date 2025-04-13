afea62a7810bd4470fd26197ed7067f921aea3e7
Roshan Chauhan <roshanranjput92@gmail.com>
2025-04-13 12:09:17 +0530

    constant.js

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,6 +1,4 @@
-// src/utils/constant.js
-
-export const USER_API_END_POINT = "http://localhost:8000/api/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/jobs"; // post
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/applicants";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/companies";
+export const USER_API_END_POINT = "https://eaii.onrender.com/api/user";
+export const JOB_API_END_POINT = "https://eaii.onrender.com/api/jobs"; // post
+export const APPLICATION_API_END_POINT = "https://eaii.onrender.com/api/applicants";
+export const COMPANY_API_END_POINT = "https://eaii.onrender.com/api/companies";
