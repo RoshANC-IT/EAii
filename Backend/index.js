755337ed4ac8b09515d94444feb73d03dfc785fd
Roshan Chauhan <roshanranjput92@gmail.com>
2025-04-13 11:58:28 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: process.env.CLIENT_URL || ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
+  origin: "https://eaii.onrender.com" || ['http://localhost:5173'],
   methods: 'GET,POST,PUT,DELETE',
   credentials: true, // Allow cookies to be sent with requests
 };
